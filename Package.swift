35aa15977b0ca0c37641c8a9fc1d73b070485df4
Ivan Vavilov <iv@redmadrobot.com>
2019-10-12 12:13:50 +0300

    Add other apple platforms

--- Package.swift
+++ Package.swift
@@ -12,7 +12,10 @@ import PackageDescription
 let package = Package(
     name: "GoldenKey",
     platforms: [
-        .iOS(.v10)
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
     ],
     products: [
         .library(
